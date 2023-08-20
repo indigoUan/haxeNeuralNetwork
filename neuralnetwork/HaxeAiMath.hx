29fbd2bbb6e87babbc94882273f1cc06aa75072e
îndigoUán <indigoUan@gmail.com>
2023-08-20 16:27:01 +0200

    Initial commit

---
+++ neuralnetwork/HaxeAiMath.hx
@@ -0,0 +1,25 @@
+package neuralnetwork;
+
+@:allow(neuralnetwork.NeuralNetwork)
+@:allow(neuralnetwork.Training)
+private class HaxeAiMath {
+    public static function dot(a:Array<Float>, b:Array<Float>):Float {
+        var result:Float = 0;
+
+        for (i in 0...a.length) {
+            for (j in 0...b.length) {
+                result += a[i] * b[j];
+            }
+        }
+
+        return result;
+    }
+
+    public static function arraySum(array:Array<Float>):Float {
+        var result:Float = 0;
+        for (i in array) {
+            result += i;
+        }
+        return result;
+    }
+}
---
+++ neuralnetwork/Training.hx
@@ -0,0 +1,22 @@
+package neuralnetwork;
+
+class Training {
+	public static function generateFromParent(parent:NeuralNetwork, randomRange:Float, children:Int):Array<NeuralNetwork> {
+		var kids:Array<NeuralNetwork> = [parent.clone()];
+		for (i in 0...children) {
+			var child:NeuralNetwork = parent.clone();
+			for (layer in child.layers) {
+				if (!layer.inputLayer) {
+					for (neuron in layer.neuronGroup) {
+						neuron.bias += randomRange * ((Math.random() - 0.5) * 2);
+						for (weight in 0...neuron.weights.length) {
+							neuron.weights[weight] += randomRange * ((Math.random() - 0.5) * 2);
+						}
+					}
+				}
+			}
+			kids.push(child);
+		}
+		return kids;
+	}
+}
