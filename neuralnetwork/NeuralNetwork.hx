375b34484aa595ba2a3d9b01fa5ad3ab9bce0ee1
îndigoUán <indigoUan@gmail.com>
2024-03-19 21:17:04 +0100

    this should make it easier to graph networks

--- neuralnetwork/NeuralNetwork.hx
+++ neuralnetwork/NeuralNetwork.hx
@@ -1,5 +1,20 @@
 package neuralnetwork;
 
+typedef NeuralNetworkGraphical = {
+	lines:Array<{
+		x1:Float,
+		y1:Float,
+		x2:Float,
+		y2:Float,
+		value:Float
+	}>,
+	dots:Array<{
+		x:Float,
+		y:Float,
+		value:Float
+	}>
+}
+
 enum OutputActivationFunction {
 	Linear;
 	Softmax;
@@ -49,6 +64,35 @@ private class NeuralNetworkInstance {
 				return normalValues;
 			}
 		}
+	}
+
+	public function graphicalMapping():NeuralNetworkGraphical {
+		var res:NeuralNetworkGraphical = {
+			lines: new Array(),
+			dots: new Array()
+		};
+
+		var widthSpace:Float = 1 / layers.length;
+		var prevX:Float = 0;
+		var prevYs:Array<Float> = [];
+		var bufferYs:Array<Float> = [];
+		for (layer in 0...layers.length) {
+			var heightSpace:Float = 1 / layers[layer].length;
+			var x:Float = widthSpace * layer;
+			for (neuron in 0...layers[layer].length) {
+				var y:Float = heightSpace * neuron;
+				bufferYs.push(y);
+
+				for (Y in 0...prevYs.length) {
+					res.lines.push({ x1: prevX, y1: prevYs[Y], x2: x, y2: y, value: layers[layer][neuron].weights[Y] });
+				}
+				res.dots.push({ x: x, y: y, value: layers[layer][neuron].bias });
+			}
+			prevX = x;
+			prevYs = bufferYs.copy();
+		}
+
+		return res;
 	}
 }
 
